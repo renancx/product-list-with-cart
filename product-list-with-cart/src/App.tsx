b90148d67bc3f495f2ecdbb771c4acba42671f23
renancx <renan.lowe@hotmail.com>
2025-01-10 09:09:03 -0300

    refactor project structure

--- product-list-with-cart/src/App.tsx
+++ product-list-with-cart/src/App.tsx
@@ -1,30 +1,9 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React loffgos to learn more
-      </p>
-    </>
-  )
+		<>
+			<h1>asdasd</h1>
+		</>
+	)
 }
 
 export default App
