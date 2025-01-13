a05bd229185d67dcac71adfbaa11a7aa20bc41e0
renancx <renan.lowe@hotmail.com>
2025-01-13 16:28:59 -0300

    add order confirmation dialog and reset cart functionality

--- product-list-with-cart/src/App.tsx
+++ product-list-with-cart/src/App.tsx
@@ -57,6 +57,11 @@ export default function App() {
 		}
 	}
 
+	const handleConfirmOrder = () => {
+		setCart({products: [], total: 0});
+		setVisible(false);
+	}
+
   	return (
 		<>
 			<div className="container">
@@ -84,12 +89,20 @@ export default function App() {
 					maskStyle={{backgroundColor: 'rgba(0, 0, 0, 0.5)'}}
 					className="cart-dialog"
 				>
-					<p className="m-0">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-					</p>
+					<h1 className="cart-dialog-title">Order Confirmed</h1>
+					<p className="cart-dialog-description">We hope you enjoy your food</p>
+					<ul className="cart-dialog-list">
+						{cart.products.map((product) => (
+							<li key={product.id} className="cart-dialog-product">
+								<p className="cart-dialog-product-name">{product.name}</p>
+								<p className="cart-dialog-product-quantity">{product.quantity}x</p>
+								<p className="cart-dialog-product-price">${product.price}</p>
+							</li>
+						))}
+					</ul>
+					<p className="cart-dialog-total">Total: ${cart.products.reduce((acc, product) => acc + (product.price * (product.quantity ?? 0)), 0)}</p>
+
+					<button className="confirm-btn" onClick={() => handleConfirmOrder() }>Start New Order</button>
 				</Dialog>
 
 				<div className="cart">
